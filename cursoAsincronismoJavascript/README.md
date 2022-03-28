2d8e33d19d3ea513bfe92899f7b826a563a9545d
Alan Garcia <48397562+AlanSeyfir@users.noreply.github.com>
2022-03-27 19:51:11 -0600

    Add course of asynchronous JS

---
+++ cursoAsincronismoJavascript/README.md
@@ -0,0 +1,23 @@
+
+## Topics which I need to understand
+Arrow functions
+Callbacks
+Promises
+Async/Await
+
+Link to platzi journey: https://github.com/AlanSeyfir/platzi-journey
+
+### Questions for exam 
+
+¿Para qué nos sirve la clase XMLHttpRequest?
+
+¿Para qué utilizamos JSON.parse(xhttp.responseText)?
+
+¿Cuál es la expresión que pausa la ejecución de la función así­ncrona y espera la resolución de la promise?
+
+**Implement our scripts in node**
+```
+"scripts": {
+    "run:my:script": "node src/code/index.js",
+},
+```
---
+++ cursoAsincronismoJavascript/src/promise/index.js
@@ -0,0 +1,38 @@
+const somethingWillHappen = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve("Hey! 😎");
+    } else {
+      reject("Fallo xd 🧨");
+    }
+  });
+};
+
+somethingWillHappen()
+  .then((response) => console.log(response))
+  .catch((err) => console.log(err));
+
+const somethingWillHappen2 = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      setTimeout(() => {
+        resolve("True");
+      }, 2000);
+    } else {
+      const error = new Error("Whoops!");
+      reject(error);
+    }
+  });
+};
+
+somethingWillHappen2()
+  .then( response => console.log(response))
+  .catch( err => console.log(err));
+
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
+  .then(response => {
+    console.log("Array of results", response);
+  })
+  .catch( err => {
+    console.error(err);
+  })
